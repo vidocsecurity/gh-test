bf663e833c4f1e6f36dc5fadd673bb51fcf9f89e
mvidoc <mikolaj@vidocsecurity.com>
2025-11-27 20:14:23 +0100

    test

---
+++ main.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { spawn } from "child_process";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => console.log(`listening on ${PORT}`));
