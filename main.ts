1bdafc0e6a702d479566946a222d4fcb58c574ea
mvidoc <mikolaj@vidocsecurity.com>
2025-11-27 20:48:16 +0100

    t

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, Worltd!");
+  res.send("Hello, world!");
 });
 
 const PORT = process.env.PORT || 5000;
